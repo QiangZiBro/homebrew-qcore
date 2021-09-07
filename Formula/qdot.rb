a47b9f9d6a8fd2cd83d39c0b98b8d2b417c3bdae
QiangZiBro <qiangzibro@gmail.com>
2021-09-07 23:26:19 +0800

    Fix: qdot

--- Formula/qdot.rb
+++ Formula/qdot.rb
@@ -1,4 +1,4 @@
-class Qdotfiles < Formula
+class Qdot < Formula
   desc "Development tools and dotfiles of QiangZiBro, supporting mac and ubuntu at the same time."
   homepage ""
   url "https://github.com/QiangZiBro/Qdotfiles/archive/refs/tags/v1.1.2.tar.gz"
