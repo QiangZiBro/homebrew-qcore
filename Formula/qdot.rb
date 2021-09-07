30bcb41e037b5334bdbee148d51857be6a3985a6
QiangZiBro <qiangzibro@gmail.com>
2021-09-07 23:56:28 +0800

    add url, but not use make install

--- Formula/qdot.rb
+++ Formula/qdot.rb
@@ -1,6 +1,7 @@
 class Qdot < Formula
   desc "Development tools and dotfiles of QiangZiBro, supporting mac and ubuntu at the same time."
   homepage ""
+  url "https://github.com/QiangZiBro/Qdotfiles/archive/refs/tags/v1.1.2.tar.gz"
   license "MIT"
   def install
     system "bash" "-c" "$(curl -fsSL https://raw.githubusercontent.com/QiangZiBro/Qdotfiles/main/scripts/bootstrap.sh)"
