6a1cf8d660370f3d67d9ec4f780767040ebe1474
QiangZiBro <qiangzibro@gmail.com>
2021-09-07 23:24:05 +0800

    first commit

---
+++ Formula/qdot.rb
@@ -0,0 +1,12 @@
+class Qdotfiles < Formula
+  desc "Development tools and dotfiles of QiangZiBro, supporting mac and ubuntu at the same time."
+  homepage ""
+  url "https://github.com/QiangZiBro/Qdotfiles/archive/refs/tags/v1.1.2.tar.gz"
+  license "MIT"
+  def install
+    system "make", "install"
+  end
+  test do
+    system "true"
+  end
+end
