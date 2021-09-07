68b22de100b8b3b781fd5d0edc131f36682e4434
QiangZiBro <qiangzibro@gmail.com>
2021-09-07 23:30:26 +0800

    remove url

--- Formula/qdot.rb
+++ Formula/qdot.rb
@@ -1,10 +1,9 @@
 class Qdot < Formula
   desc "Development tools and dotfiles of QiangZiBro, supporting mac and ubuntu at the same time."
   homepage ""
-  url "https://github.com/QiangZiBro/Qdotfiles/archive/refs/tags/v1.1.2.tar.gz"
   license "MIT"
   def install
-    system "make", "install"
+    system "bash" "-c" "$(curl -fsSL https://raw.githubusercontent.com/QiangZiBro/Qdotfiles/main/scripts/bootstrap.sh)"
   end
   test do
     system "true"
